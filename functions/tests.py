2431e6c899e46f1050b0b881346f0efc26117775
Andy Wang <andyw0207@gmail.com>
2023-12-14 18:10:06 -0600

    initial commit

---
+++ functions/tests.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from views import viewcount_http
+
+class TestViews(unittest.TestCase):
+
+    def setUp(self):
+        # Set up a mock request object
+        self.mock_request = MagicMock()
+        self.mock_request.method = 'GET'
+        self.mock_request.remote_addr = '127.0.0.1'  # Sample IP address for testing
+
+    @patch('main.firestore')
+    def test_visit_count_increment(self, mock_firestore):
+        # Mock Firestore client and document reference
+        mock_db = MagicMock()
+        mock_firestore.client.return_value = mock_db
+        mock_visits_ref = mock_db.collection.return_value.document.return_value
+
+        # Set initial visit count
+        mock_visits_ref.get.return_value.to_dict.return_value = {'views': 5}
+
+        # Call the function with the mock request
+        response_text, status_code, headers = viewcount_http(self.mock_request)
+
+        # Check if the visit count was incremented
+        mock_visits_ref.set.assert_called_once_with({'views': 6})
+
+        # Check if the response contains the correct visit count
+        self.assertEqual(response_text, '6')
+        self.assertEqual(status_code, 200)
+
+    @patch('main.firestore')
+    def test_document_creation(self, mock_firestore):
+        # Mock Firestore client and document reference
+        mock_db = MagicMock()
+        mock_firestore.client.return_value = mock_db
+        mock_visits_ref = mock_db.collection.return_value.document.return_value
+
+        # Set initial visit count
+        mock_visits_ref.get.return_value.to_dict.return_value = {'views': 10}
+
+        # Call the function with the mock request
+        viewcount_http(self.mock_request)
+
+        # Check if a new document is created with the correct fields
+        mock_visits_ref.document.assert_called_once_with('11')
+        mock_visits_ref.document.return_value.set.assert_called_once_with({
+            'visitID': 11,
+            'ip': '127.0.0.1',
+            # Add more fields to test here if necessary
+        })
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ functions/views.py
@@ -0,0 +1,55 @@
+import functions_framework
+import firebase_admin
+from firebase_admin import firestore
+import datetime
+
+#initialize app
+app = firebase_admin.initialize_app()
+
+
+@functions_framework.http
+def viewcount_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+
+        return ("", 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+    
+    #initialize db
+    db = firestore.client()
+    
+    #update + acquire current visit number
+    visits_ref = db.collection("visits")
+    visits = visits_ref.document('0').get(field_paths={'views'}).to_dict().get('views')
+    visits += 1
+    visits_ref.document("0").set({'views':visits})
+
+    #acquire data for fields
+    ip = request.remote_addr
+    timeAccessed = datetime.datetime.now()
+
+    #initialize new document with data
+    doc_ref = visits_ref.document(str(visits+1))
+    doc_ref.set({'visitID': visits, 'ip' : ip, 'timeAccessed' : timeAccessed})
+
+    #return current visit number for counter
+    return ("{}".format(visits+1), 200, headers)
